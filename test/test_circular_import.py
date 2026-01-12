1ca0782d0d23ec0c3c2adf74bd4616d2e691c229
Miek Messerschmidt <mmesserschmiddt@gmail.com>
2026-01-12 10:29:11 +0200

    restructure

--- test/test_circular_import.py
+++ test/test_circular_import.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_circular_import_detection():
     """Test that circular imports are detected and raise an appropriate error."""
-    root = Path("./test/test_configs/conf_circular_import/root.toml")
+    root = Path("./test/error_configs/conf_circular_import/root.toml")
     
     # The circular import chain is: root -> a -> b -> a
     # This should raise an error (either RecursionError or a custom circular import error)
